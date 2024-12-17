e143fedc4c09c30370c901a46b15a7ea606a736f
fairuz <sakhiyfairuz@gmail.com>
2024-12-17 10:17:42 +0700

    product page testing

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -1,7 +1,22 @@
-export default function ProductPage() {
-    return (
-        <div>
-            Product Page
-        </div>
-    )
+import { fetchAll } from "./server";
+import TestButton from "./ProductCreateForm";
+
+export default async function ProductPage() {
+  console.log((await fetchAll()).data);
+
+    const data = await (await fetchAll()).data
+
+  return (
+    <div>
+      <div>Product Page</div>
+      <br />
+      <div>
+        {data.map((i) => (
+            <div key={i.id} >{i.name}</div>
+        ))}
+      </div>
+      <br />
+      <TestButton />
+    </div>
+  );
 }
