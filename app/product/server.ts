efaac2af5ab4711be7a48b3486a1a405141cc2b1
fairuz <sakhiyfairuz@gmail.com>
2024-12-17 10:17:31 +0700

    added server actions for product fetching and creating

---
+++ app/product/server.ts
@@ -0,0 +1,22 @@
+'use server'
+
+import prisma from "@/lib/db"
+import { revalidatePath } from "next/cache"
+
+export async function fetchAll() {    
+    const dbData = await prisma.product.findMany()
+    
+    return {
+        data: dbData
+    }
+}
+
+export async function create(name: string) {
+    await prisma.product.create({
+        data: {
+            name: name
+        }
+    })
+
+    revalidatePath('/product')
+}
